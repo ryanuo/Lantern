a1db62e0592f6b436cbb66d99d95e30066b75115
R | blog <66169324+w1577791638@users.noreply.github.com>
2021-03-13 17:35:52 +0800

    docs: 加入js

--- dl.js
+++ dl.js
@@ -1,4 +1,8 @@
-
+// 灯笼样式
+var headd = document.querySelector('header')
+var ddiv = document.createElement('div')
+headd.appendChild(ddiv)
+ddiv.className ='dengl'
 var heads = '<div class="d-box"><div class="d1"><span></span><span><p></p></span><ul><li></li><li></li><li><span></span></li><li></li><li></li></ul></div><div class="d2"><span></span><span><p></p></span><ul><li></li><li></li><li><span></span></li><li></li><li></li></ul></div></div><div class="d-box1"><div class="d1"><span></span><span><p></p></span><ul><li></li><li></li><li><span></span></li><li></li><li></li></ul></div><div class="d2"><span></span><span><p></p></span><ul><li></li><li></li><li><span></span></li><li></li><li></li></ul></div></div>'
 var headers = document.querySelector('.dengl')
 headers.innerHTML = heads;
